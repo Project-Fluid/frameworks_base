1c18e4d93f8b2ebe25535688e958c877e12b93a6
Anushek Prasal <anushekprasal@gmail.com>
2019-09-23 22:13:27 +0530

    base: Update some icons to MD2
    Change-Id: Ia938aa767fdf42d79c611c523f0e21fd41cf3e45
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/CastControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/CastControllerImpl.java
@@ -181,10 +181,6 @@ public class CastControllerImpl implements CastController {
         synchronized (mProjectionLock) {
 
             if (mProjection != null && mProjection.getPackageName().equals(
-                    RecorderConstants.APP_PACKAGE_NAME)){
-                mProjection = null;
-            }
-            if (mProjection != null && mProjection.getPackageName().equals(
                     StitchImageUtility.STITCHIMAGE_APP_PACKAGE_NAME)){
                 mProjection = null;
             }
