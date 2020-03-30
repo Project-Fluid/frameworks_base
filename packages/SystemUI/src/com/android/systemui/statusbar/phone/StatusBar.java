b21b6d95bcbe149759356953cb9bae43fbce0507
rituj <ritujbeniwal@gmail.com>
2020-03-30 16:39:49 +0530

    SystemUI: Move brightness slider to the bottom
    * Move the complete implementation to QS footer
    * Make it accessible on mini-statusbar as well
    * Add auto-brightness button
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Iceb4d429554be85b0cd693cb49ec7e7d534e271f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1209,7 +1209,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                             .withPlugin(QS.class)
                             .withDefault(this::createDefaultQSFragment)
                             .build());
-            mBrightnessMirrorController = new BrightnessMirrorController(mStatusBarWindow,
+            mBrightnessMirrorController = new BrightnessMirrorController(mContext, mStatusBarWindow,
                     (visible) -> {
                         mBrightnessMirrorVisible = visible;
                         updateScrimController();
@@ -1218,7 +1218,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 QS qs = (QS) f;
                 if (qs instanceof QSFragment) {
                     mQSPanel = ((QSFragment) qs).getQsPanel();
-                    mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+                    ((QSFragment) qs).getQsFooter().setBrightnessMirror(mBrightnessMirrorController);
                 }
             });
         }
