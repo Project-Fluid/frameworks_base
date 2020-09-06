30902deb32e92df56d8a8ce78ab43835099448cb
cjybyjk <cjybyjk@zjnu.edu.cn>
2020-09-07 01:44:05 +0800

    base: make app volume rows optional [1/2]
    Change-Id: I5c02019a7cace127da41d72b386ad6f512871600

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5150,6 +5150,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
