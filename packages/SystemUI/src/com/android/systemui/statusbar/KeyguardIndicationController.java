fde7df30fb3014fb893c22d951fa3ad56e69c953
karthick111 <karthick111@users.noreply.github.com>
2020-10-18 21:40:54 +0000

    BatteryService: Add VOOC charging support
    * Realme devices support non standard charger type, i.e vooc charger that has its own crazy charge speeds.
    * Luckily vooc charger can be determined upon the value of one node, Let's use that and update user when vooc charger is conected and vooc charging is in progress.
    * Also guard this via a overlay flag and set it to false by default to preserve the default aosp behaviour for non standard chargers if user uses them.
    * [@dev-harsh1998]: Forward Port to Android R's fuelgauge implementation.
    
    Co-authored-by: Harshit Jain <god@hyper-labs.tech>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -541,10 +541,16 @@ public class KeyguardIndicationController implements StateListener,
                             ? R.string.keyguard_indication_dash_charging_time
                             : R.string.keyguard_plugged_in_dash_charging;
                     break;
+
                 case BatteryStatus.CHARGING_WARP:
                     chargingId = hasChargingTime
                             ? R.string.keyguard_indication_warp_charging_time
                             : R.string.keyguard_plugged_in_warp_charging;
+                    break;
+                case BatteryStatus.CHARGING_VOOC:
+                    chargingId = hasChargingTime
+                            ? R.string.keyguard_indication_vooc_charging_time
+                            : R.string.keyguard_plugged_in_vooc_charging;
                     break;
                 case BatteryStatus.CHARGING_SLOWLY:
                     chargingId = hasChargingTime
