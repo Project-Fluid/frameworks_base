13fc8518d19b82d87e1c34838e262fff15dc355d
ender1324 <ender1324@abv.bg>
2020-12-05 18:19:09 +0200

    Revert "ScreenshotSound: Fix playing screenshot's sound when camera's sounds off."
    - Wacks screenshot sound
    
    This reverts commit 4fea5778a86fb1922c12023be4727b9d7dd4dd8e.

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -56,8 +56,6 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.media.MediaActionSound;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
@@ -231,7 +229,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     private float mCornerSizeX;
     private float mDismissDeltaY;
 
-    private Ringtone  mScreenshotSound;
+    private MediaActionSound mCameraSound;
 
     private int mNavMode;
     private int mLeftInset;
@@ -326,10 +324,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
         mFastOutSlowIn =
                 AnimationUtils.loadInterpolator(mContext, android.R.interpolator.fast_out_slow_in);
 
-        // Setup the Screenshot sound
-        mScreenshotSound= RingtoneManager.getRingtone(mContext,
-                    Uri.parse("file://" + "/system/media/audio/ui/camera_click.ogg"));
-
         // Store UI background executor
         mUiBgExecutor = uiBgExecutor;
 
@@ -341,6 +335,10 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         // Initialize current foreground package name
         mTaskListener.onTaskStackChanged();
+
+        // Setup the Camera shutter sound
+        mCameraSound = new MediaActionSound();
+        mCameraSound.load(MediaActionSound.SHUTTER_CLICK);
     }
 
     @Override // ViewTreeObserver.OnComputeInternalInsetsListener
@@ -715,7 +713,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     private void saveScreenshotAndToast(Consumer<Uri> finisher) {
         // Play the shutter sound to notify that we've taken a screenshot
         mScreenshotHandler.post(() -> {
-            mScreenshotSound.play();
+            mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
         });
 
         saveScreenshotInWorkerThread(finisher, new ActionsReadyListener() {
@@ -877,7 +875,8 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                 });
 
                 // Play the shutter sound to notify that we've taken a screenshot
-                        mScreenshotSound.play();
+                mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+
                 mScreenshotPreview.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotPreview.buildLayer();
                 mScreenshotAnimation.start();
