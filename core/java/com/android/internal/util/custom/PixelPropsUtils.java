f7069e92fd9829682f24f5684d9fecb69bc6c8da
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-28 22:12:40 +0530

    PixelPropsUtils: Add netflix to spoofing list
     * unlock HDR support
    
    Change-Id: If38861f351308658509e009e1841ef556b4f7542

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -37,7 +37,8 @@ public class PixelPropsUtils {
     private static final String[] extraPackagesToChange = {
             "com.android.chrome",
             "com.android.vending",
-            "com.breel.wallpapers20"
+            "com.breel.wallpapers20",
+            "com.netflix.mediaclient"
     };
 
     static {
