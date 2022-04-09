b630d03f439cdb51bbab538b76d3a2e241c4c878
jhenrique09 <jhsv09@gmail.com>
2022-04-09 19:31:31 +0000

    PixelPropsUtils: Update fingerprint to April release
    Change-Id: Ic31db0c267571c056556a2469917d725a69baf50

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -49,7 +49,7 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "redfin");
         propsToChange.put("PRODUCT", "redfin");
         propsToChange.put("MODEL", "Pixel 5");
-        propsToChange.put("FINGERPRINT", "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/redfin/redfin:12/SP2A.220405.003/8210211:user/release-keys");
     }
 
     public static void setProps(String packageName) {
